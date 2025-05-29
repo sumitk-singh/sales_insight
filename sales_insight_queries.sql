bc20949567ab8563e03d3cd24ca79107e92ec9ec
SUMIT SINGH <76554602+sumitk-singh@users.noreply.github.com>
2025-05-29 21:26:41 +0530

    Update sales_insight_queries.sql

--- sales_insight_queries.sql
+++ sales_insight_queries.sql
@@ -8,3 +8,16 @@ SELECT COUNT(*) AS RegionCount FROM Regions;
 
 -- 3. List managers in alphabetical order:
 SELECT Manager FROM Regions ORDER BY Manager;
+
+-- 4. Find customers who joined after 2022-01-01:
+SELECT * FROM Customers WHERE JoinDate > '2022-01-01';
+
+-- 5. List all products in the “Electronics” category:
+SELECT * FROM Products WHERE Category IN( 'Electronics');
+
+-- 6. List all products that do not belong to “Electronics” category:
+SELECT * FROM Products WHERE Category NOT IN ('Electronics');
+
+
+
+

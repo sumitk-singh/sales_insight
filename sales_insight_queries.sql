2207874fe90324598b1558554ae433e6198112dd
SUMIT SINGH <76554602+sumitk-singh@users.noreply.github.com>
2025-05-29 21:09:24 +0530

    Add files via upload

---
+++ sales_insight_queries.sql
@@ -0,0 +1,10 @@
+
+
+-- 1. List all regions:
+SELECT * FROM Regions;
+
+-- 2. Count number of regions:
+SELECT COUNT(*) AS RegionCount FROM Regions;
+
+-- 3. List managers in alphabetical order:
+SELECT Manager FROM Regions ORDER BY Manager;
